a7b8add5b1ede8cd878751baaf9de567fd711822
thisjheok <jhjang3344@gmail.com>
2026-08-05 16:28:10 +0900

    feat: resize(): fill 0 if shape mismatch

--- Numcpp/Main.cpp
+++ Numcpp/Main.cpp
@@ -4,20 +4,14 @@
 int main()
 {
 	Ndarray<std::size_t> ndarr = {
-		{3,3,3},
-		{2,2,6,1,3,6,1,0,1,2,2,6,1,3,6,1,0,1,2,2,6,1,3,6,1,0,1}
+		{2,3},
+		{2,2,6,1,3,6}
 	};
 
 	std::cout << "original Ndarray: " << '\n';
 	ndarr.print_ndarray();
 	std::cout << '\n';
 
-	Ndarray<std::size_t> reshaped_ndarr = ndarr.reshape({ 3,3 });
-	
-	std::cout << "reshaped Ndarray: " << '\n';
-	reshaped_ndarr.print_ndarray();
-	std::cout << '\n';
-
-	std::size_t result = ndarr.item({ 1,2,2 });
-	std::cout << "result: " << result;
+	Ndarray<std::size_t> resized_arr = ndarr.resize({ 3,3 });
+	resized_arr.print_ndarray();
 }
--- Numcpp/Ndarray.h
+++ Numcpp/Ndarray.h
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <typeinfo>
 #include <type_traits>
+#include <memory>
 
 // ndarray의 사이즈, 생성자, n번째 원소에 접근, 출력, 범위 검사
 // ndarry 생성 시 몇차원인지 어떻게 파악할까 => shape 
@@ -16,9 +17,11 @@ public:
 
 	std::size_t size() const;
 	std::size_t ndim() const;
+	std::size_t shape_size() const;
 
 	Ndarray<T> copy() const;
 	Ndarray<T> reshape(const std::vector<std::size_t> shape) const;
+	Ndarray<T> resize(const std::vector<std::size_t> shape);
 
 	void fill(const T& value); 
 	void print_stride();
@@ -73,6 +76,17 @@ std::size_t Ndarray<T>::ndim() const
 	return shape_.size();
 }
 
+template <typename T>
+std::size_t Ndarray<T>::shape_size() const
+{
+	std::size_t result = 1;
+	for (auto elem : shape_)
+	{
+		result *= elem;
+	}
+	return result;
+}
+
 // Fill the array with a scalar value.
 template <typename T>
 void Ndarray<T>::fill(const T& value)
@@ -97,7 +111,6 @@ Ndarray<T> Ndarray<T>::reshape(const std::vector<std::size_t> shape) const
 
 	return reshaped_ndarr;
 }
-
 
 // Debugging function to print the stride of the ndarray
 template <typename T>
@@ -153,10 +166,36 @@ template <typename T>
 T Ndarray<T>::item(const std::vector<std::size_t> indices) const
 {
 	std::size_t index = 0;
+	std::vector<std::size_t> data_copy = data_;
+
 	for (std::size_t i = 0; i < indices.size(); ++i)
 	{
 		index += indices[i] * stride_[i];
 	}
 	std::cout << "index:" << index << '\n';
-	return data_[index];
+	return data_copy[index];
 }
+
+template <typename T>
+Ndarray<T> Ndarray<T>::resize(const std::vector<std::size_t> shape)
+{
+	// TODO: 크기가 크다면 0으로 채울 것
+	
+	std::size_t shape_mul = 1;
+
+	for (auto elem : shape)
+	{
+		shape_mul *= elem;
+	}
+
+	if (shape_mul > shape_size())
+	{
+		for (std::size_t i = 0; i < shape_mul - shape_size(); ++i)
+		{
+			data_.push_back(0);
+		}
+	}
+	return reshape(shape);
+}
+
+// TODO: stride 계산 함수 
